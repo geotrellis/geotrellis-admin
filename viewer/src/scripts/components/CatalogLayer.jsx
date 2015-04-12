037576a791c8c7004dfe72581fd011a5ee877e23
Mark Daniel <mark.r.daniel@gmail.com>
2015-04-12 18:25:57 -0500

    reformatted CatalogLayer.jsx

--- viewer/src/scripts/components/CatalogLayer.jsx
+++ viewer/src/scripts/components/CatalogLayer.jsx
@@ -8,7 +8,7 @@ var React = require('react');
 var Table = require("react-bootstrap/Table");
 var Button = require("react-bootstrap/Button");
 var _     = require("underscore");
-
+              
 
 /**
  * Props:
@@ -17,83 +17,87 @@ var _     = require("underscore");
  * url
  */
 var CatalogLayer = React.createClass({
-    getInitialState: function() {
-                         var layer = this.props.entries[0].layer;
+  getInitialState: function() {
+    var layer = this.props.entries[0].layer;
 
-                         $.get(this.props.url + "/catalog/" + layer.name + "/" + layer.zoom + "/bands", 
-                             function(bands) {
-                                 if (this.isMounted()) { this.setState({'bands': bands }) }
-                             }.bind(this)
-                             );
+    $.get(this.props.url + "/catalog/" + layer.name + "/" + layer.zoom + "/bands", 
+      function(bands) {
+        if (this.isMounted()) { this.setState({'bands': bands }) }
+      }.bind(this)
+    );
 
-                         return {'bands': []}; // list of bands in this layer
-                     },
+    return {'bands': []}; // list of bands in this layer
+  },
 
-    handleSelectLayer: function() {
+  handleSelectLayer: function() {
+    var zoom = this.refs.zoom.getDOMNode().value.trim();
+    if (this.refs.band == null)//if no node for empty band, provide an empty value
+      var band = "";
+    else
+      var band = this.refs.band.getDOMNode().value.trim();
+    
+    var bands;
 
-                           var zoom = this.refs.zoom.getDOMNode().value.trim();
-                           if (this.refs.band == null){//if no node for empty band, provide an empty value
-                               var band = "";
-                           }else{
-                               var band = this.refs.band.getDOMNode().value.trim();
-                           }
-                           var bands;
+    if (band == "")
+      bands = {};
+    else
+      bands = {'time': band };
 
-                           if (band == "")
-                               bands = {};
-                           else
-                               bands = {'time': band };
-                           var entry = _.find(this.props.entries, function(e) {return e.layer.zoom == zoom});
+    var entry = _.find(this.props.entries, function(e) {return e.layer.zoom == zoom});
 
-                           this.props.active.set(
-                                   {
-                                       'entry': entry,
-                               'band': bands 
-                                   } 
-                                   );
-                       },
+    this.props.active.set(
+       {
+         'entry': entry,
+         'band': bands 
+       } 
+    );
+  },
 
 
-    render: function () {
-                var self = this;
-                var entries = this.props.entries;
-                var entry = this.props.entries[0];
-                var layer = entry.layer;
+  render: function () {
+    var self = this;
+    var entries = this.props.entries;
+    var entry = this.props.entries[0]; // just give me the first one (heighest zoom level)
+    var layer = entry.layer;
 
-                var zoomOptions = _.map(entries, function(e) {
-                    return <option value={e.layer.zoom}>{e.layer.zoom}</option>;
-                })
+    var zoomOptions = _.map(entries, function(e) {
+      return <option value={e.layer.zoom}>{e.layer.zoom}</option>;
+    })
 
-                var bandOptions = _.map(this.state.bands.time, function(time) {    
-                    return <option value={time}>{time}</option>;        
-                });
+    var bandOptions = _.map(this.state.bands.time, function(time) {    
+      return <option value={time}>{time}</option>;        
+    });
 
-                if (bandOptions.length > 0){ //provide a selector for bands with values, none for those without
-                    return (        
-                            <tr>
-                            <td>
-                            <Button onClick={this.handleSelectLayer} bsStyle="primary" bsSize="xsmall">{ this.props.layerName }</Button>        
-                            </td>
-                            <td>
-                            <select ref="zoom">{ zoomOptions }</select>
-                            </td>
-                            <td>
-                            <select ref="band">{ bandOptions }</select>
-                            </td>
-                            </tr>);
-                }
-                else{
-                    return(
-                            <tr>
-                            <td>
-                            <Button onClick={this.handleSelectLayer} bsStyle="primary" bsSize="xsmall">{ this.props.layerName }</Button>        
-                            </td>
-                            <td>
-                            <select ref="zoom">{ zoomOptions }</select>
-                            </td>
-                            </tr>);
-                }
-            }  
+    if (bandOptions.length > 0){ //provide a selector for bands with values, none for those without
+      return (        
+          <tr>
+            <td>
+              <Button onClick={this.handleSelectLayer} bsStyle="primary" bsSize="xsmall">{ this.props.layerName }</Button>        
+            </td>
+            <td>
+              <select ref="zoom">{ zoomOptions }</select>
+            </td>
+            <td>
+                <select ref="band">{ bandOptions }</select>
+            </td>
+          </tr>);
+    }else{
+      return(
+           <tr>
+              <td>
+                <Button onClick={this.handleSelectLayer} bsStyle="primary" bsSize="xsmall">{ this.props.layerName }</Button>        
+              </td>
+              <td>
+                  <select ref="zoom">{ zoomOptions }</select>
+              </td>
+            </tr>);
+    }
+
+  }  
 });
 
 module.exports = CatalogLayer;
+
+
+
+
