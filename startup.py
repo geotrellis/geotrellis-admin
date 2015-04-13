f63750e6cf2252071256063ed724ea15e4eae513
SageMoore <sagemoore@utexas.edu>
2015-04-12 22:24:49 -0500

    added temporal support to startup.py

--- startup.py
+++ startup.py
@@ -2,8 +2,9 @@ import argparse
 import subprocess
 
 parser = argparse.ArgumentParser(description="Runs all of the ingest stuff")
-parser.add_argument("data", help="file path to data")
+parser.add_argument("data", help="File path to data")
 parser.add_argument("type", help="type of data being ingested")
+parser.add_argument("table", help="Name of table")
 parser.add_argument("layer_name", help="Name of the layer")
 args = parser.parse_args()
 
@@ -13,7 +14,12 @@ subprocess.call("./sbt 'project ingest' assembly", shell=True)
 
 jar = "ingest/target/scala-2.10/gt-admin-ingest-assembly-0.1.0-SNAPSHOT.jar"
 data = "file:" + args.data
-table = args.type
+table = args.table
+
+data_type = "AccumuloIngestCommand"
+if(args.type == "temporal"):
+    data_type = "NexIngest"
+
 layer = args.layer_name
 crs = "EPSG:3857"
 pyramid = "true"
@@ -23,7 +29,7 @@ subprocess.call("zip -d" + jar + "META-INF/ECLIPSEF.RSA", shell=True)
 subprocess.call("zip -d" + jar + "META-INF/ECLIPSEF.SF", shell=True)
 
 subprocess.call("echo spark-submit \
---class geotrellis.admin.ingest.AccumuloIngestCommand \
+--class geotrellis.admin.ingest." + data_type + " \
 " + jar + " \
 --instance gis --user root --password secret --zookeeper localhost \
 --crs " + crs + " \
