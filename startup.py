fe8e8e8e0212fbabdf29ecff111daed5ef1d263c
SageMoore <sagemoore@utexas.edu>
2015-04-22 19:25:47 -0500

    set default values for tile and layer names

--- startup.py
+++ startup.py
@@ -4,8 +4,8 @@ import subprocess
 parser = argparse.ArgumentParser(description="Runs all of the ingest stuff")
 parser.add_argument("data", help="File path to data")
 parser.add_argument("type", help="type of data being ingested")
-parser.add_argument("table", help="Name of table")
-parser.add_argument("layer_name", help="Name of the layer")
+parser.add_argument("--table", help="Name of table")
+parser.add_argument("--layer_name", help="Name of the layer")
 args = parser.parse_args()
 
 
@@ -17,10 +17,18 @@ data = "file:" + args.data
 table = args.table
 
 data_type = "AccumuloIngestCommand"
-if(args.type == "temporal"):
+if args.type == "space-time":
     data_type = "NexIngest"
 
-layer = args.layer_name
+table = "default_table"
+if args.table:
+    table = args.table
+
+layer = "default_layer"
+if args.layer_name:
+    layer = args.layer_name
+
+
 crs = "EPSG:3857"
 pyramid = "true"
 clobber = "true"
@@ -28,18 +36,9 @@ clobber = "true"
 subprocess.call("zip -d" + jar + "META-INF/ECLIPSEF.RSA", shell=True)
 subprocess.call("zip -d" + jar + "META-INF/ECLIPSEF.SF", shell=True)
 
-subprocess.call("echo spark-submit \
---class geotrellis.admin.ingest." + data_type + " \
-" + jar + " \
---instance gis --user root --password secret --zookeeper localhost \
---crs " + crs + " \
---pyramid " + pyramid + " --clobber " + clobber + " \
---input " + data + " \
---layerName " + layer + " \
---table " + table, shell=True)
 
 subprocess.call("spark-submit \
---class geotrellis.admin.ingest.AccumuloIngestCommand \
+--class geotrellis.admin.ingest." + data_type + " \
 " + jar + " \
 --instance gis --user root --password secret --zookeeper localhost \
 --crs " + crs + " \
