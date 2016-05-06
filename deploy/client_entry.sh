bb821bc7e2695d22244df8f0bb1bf7e7c8703f9f
Colin Woodbury <colingw@gmail.com>
2016-05-06 10:00:30 -0700

    Use host names to communicate between composed Docker images
    - "link environment variables" are no longer the recommended method for
    connecting composed services. We can refer to each service by hostname instead.
    - Since the ports used by each service are already hardcoded in `docker-compose.yml`,
    we shouldn't feel bad about hardcoding them in the nginx.conf

--- deploy/client_entry.sh
+++ deploy/client_entry.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-echo "IP ${GEOTRELLISADMIN_ADMINSERVER_1_PORT_8080_TCP_ADDR}"
-echo "PORT ${GEOTRELLISADMIN_ADMINSERVER_1_PORT_8080_TCP_PORT}"
-
-sed -i.bak "s/{GT-ADMIN-ADDR}/${GEOTRELLISADMIN_ADMINSERVER_1_PORT_8080_TCP_ADDR}/g" /etc/nginx/nginx.conf
-sed -i.bak "s/{GT-ADMIN-PORT}/${GEOTRELLISADMIN_ADMINSERVER_1_PORT_8080_TCP_PORT}/g" /etc/nginx/nginx.conf
+echo "Starting nginx..."
 
 nginx -g "daemon off;"
 "$@"
