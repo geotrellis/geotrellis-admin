243cb1d3243d15465a98992f1099e0b6de51003b
Colin Woodbury <colingw@gmail.com>
2016-05-27 14:46:11 -0700

    (client) Experiencing macro expansion errors on compilation

--- client/src/main/scala/geotrellis/admin/client/components/modal/LayerList.scala
+++ client/src/main/scala/geotrellis/admin/client/components/modal/LayerList.scala
@@ -16,71 +16,94 @@ import japgolly.scalajs.react.vdom.prefix_<^._
 
 object LayerList {
 
-  class Backend($: BackendScope[ModelProxy[LayerModel], Unit]) {
-    def onMount(props: ModelProxy[LayerModel]) =
-      Callback.when(props().layers.isEmpty)(props.dispatch(RefreshLayers))
+  case class Selection(value: js.UndefOr[ReactNode] = {
+    println("INITIALIZED AS UNDEF")
+    js.undefined
+  })
 
-    def render(props: ModelProxy[LayerModel]) = {
-      <.div(
+  class LayerBackend($: BackendScope[ModelProxy[LayerModel], Selection]) {
+
+    def onMount(proxy: ModelProxy[LayerModel]) =
+      Callback.when(proxy().layers.isEmpty)(proxy.dispatch(RefreshLayers))
+
+//    def onSelect(layer: Option[LayerDescription]) =
+//      proxy.dispatch(SelectLayer(layer))
+
+    def onChange(options: Seq[LayerDescription], value: ReactNode): Callback = {
+      println("ON CHANGE!")
+
+      val selection = JSON.parse(JSON.stringify(value))
+      val layerIndex: Int = selection.value.asInstanceOf[Int]
+
+      $.modState(_.copy(value = value)) >>
+      $.props >>= { proxy: ModelProxy[LayerModel] =>
+        proxy.dispatch(SelectLayer(Try(options(layerIndex)).toOption))
+      }
+    }
+
+    def render(proxy: ModelProxy[LayerModel]) = {
+        <.div(
         <.h3("Layer"),
-        props().layers.renderPending(_ > 500, _ => <.p("Loading...")),
-        props().layers.renderFailed(ex => <.p("Failed to load")),
-        props().layers.render(layers => {
-          <.div(
+        proxy().layers.renderPending(_ > 500, _ => <.p("Loading...")),
+        proxy().layers.renderFailed(ex => <.p("Failed to load")),
+        proxy().layers.render(layers => {
+          <.div({
+            /*
             LayerForm(
               LayerForm.Props(
                 options = layers.sortBy(_.name),
-                onSelect = layer => props.dispatch(SelectLayer(layer))
+                onSelect = layer => proxy.dispatch(SelectLayer(layer))
               )
             )
-          )
+             */
+
+            // TODO The state is resetting itself to `undefined`.
+
+            println(s"STATE: ${state.value}")
+
+            val layDs = layers.sortBy(_.name)
+            val options = layDs.zipWithIndex.map { case (option, index) =>
+              ValueOption[ReactNode](value = index, label = option.name)
+            }.toJSArray
+
+            Select(
+              options = options
+              value = state.value,
+              onChange = { rn: ReactNode => onChange(layDs, rn) },
+              placeholder = "Select a layer to view...".asInstanceOf[ReactNode]
+            )()
+          })
         })
       )
     }
   }
 
   private val layerViewer = ReactComponentB[ModelProxy[LayerModel]]("LayerViewer")
-    .renderBackend[Backend]
+    .initialState(Selection())
+    .renderBackend[LayerBackend]
     .componentDidMount(scope => scope.backend.onMount(scope.props))
     .build
 
-  def apply(props: ModelProxy[LayerModel]) = layerViewer(props)
+  def apply(proxy: ModelProxy[LayerModel]) = layerViewer(proxy)
 }
 
+/*
 object LayerForm {
 
   case class Props(options: Seq[LayerDescription], onSelect: Option[LayerDescription] => Callback)
-  case class State(value: js.UndefOr[ReactNode] = js.undefined)
 
   class Backend($: BackendScope[Props, State]) {
 
-    def onChange(value: ReactNode) = {
-      val selection = JSON.parse(JSON.stringify(value))
-      val layerIndex: Int = selection.value.asInstanceOf[Int]
-
-      $.modState(_.copy(value = value)) >>
-        $.props >>= { (props: Props) => props.onSelect(Try(props.options(layerIndex)).toOption) }
-    }
-
     def render(state: State, props: Props) = {
-      val options =
-        props.options.zipWithIndex.map { case (option, index) =>
-          ValueOption[ReactNode](value = index, label = option.name)
-        }.toJSArray
 
-      Select(
-        options = options,
-        value = state.value,
-        onChange = onChange _,
-        placeholder = "Select a layer to view...".asInstanceOf[ReactNode]
-      )()
     }
   }
 
-  private val component = ReactComponentB[Props]("ReactSelectDemo")
-    .initialState(State())
+  private val component = ReactComponentB[Props]("SelectForm")
+
     .renderBackend[Backend]
     .build
 
   def apply(props: Props) = component(props)
 }
+ */
